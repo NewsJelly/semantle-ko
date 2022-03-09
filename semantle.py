4692cfa37d719632520e0a4659dd03cbf8d1c631
Johannes Gätjen <johannesgaetjen@gmail.com>
2022-03-09 21:08:42 +0100

    launch app with waitress

--- semantle.py
+++ semantle.py
@@ -1,5 +1,5 @@
 import pickle
-
+import waitress
 from flask import (
     Flask,
     send_file,
@@ -11,6 +11,16 @@ import word2vec
 from process_similar import dump_nearest
 
 app = Flask(__name__)
+# todo: dependent on current day / check if already exists / start job every 24 h
+app.start_idx = 1002
+print("loading valid nearest")
+with open('data/valid_nearest.dat', 'rb') as f:
+    valid_nearest_words, valid_nearest_vecs = pickle.load(f)
+with open('data/secrets.txt', 'r', encoding='utf-8') as f:
+    secrets = [l.strip() for l in f.readlines()]
+app.secret = secrets[app.start_idx]
+print("dumping nearest")
+app.nearest = dump_nearest(app.secret, valid_nearest_words, valid_nearest_vecs)
 
 
 @app.route('/')
@@ -33,36 +43,23 @@ def send_static(path):
 def get_guess(day: int, word: str):
     rtn = {"guess": word}
     # check most similar
-    if word in nearest:
-        rtn["sim"] = nearest[word][1]
-        rtn["rank"] = nearest[word][0]
+    if word in app.nearest:
+        rtn["sim"] = app.nearest[word][1]
+        rtn["rank"] = app.nearest[word][0]
     else:
         try:
-            rtn["sim"] = word2vec.similarity(secret, word)
+            rtn["sim"] = word2vec.similarity(app.secret, word)
             rtn["rank"] = "(kalt)"
         except KeyError:
             return 'Word unknown', 404
     return jsonify(rtn)
 
 
-
 @app.route('/similarity/<int:day>')
 def get_similarity(day: int):
-    nearest_dists = sorted([v[1] for v in nearest.values()])
-    print(nearest_dists)
+    nearest_dists = sorted([v[1] for v in app.nearest.values()])
     return jsonify({"top": nearest_dists[-2], "top10": nearest_dists[-12], "rest": nearest_dists[0]})
 
 
-if __name__ == '__main__':
-    # todo: dependent on current day / check if already exists / start job every 24 h
-    start_idx = 1001
-    print("loading valid nearest")
-    with open('data/valid_nearest.dat', 'rb') as f:
-        valid_nearest_words, valid_nearest_vecs = pickle.load(f)
-    with open('data/secrets.txt', 'r', encoding='utf-8') as f:
-        secrets = [l.strip() for l in f.readlines()]
-    secret = secrets[start_idx]
-    print("dumping nearest")
-    nearest = dump_nearest(secret, valid_nearest_words, valid_nearest_vecs)
-    print("launching app")
-    app.run(host='0.0.0.0', port=5000)
+print("starting waitress")
+waitress.serve(app, host='0.0.0.0', port=5000, threads=1)

7cd1e5855c3d0aeeb3d947e635996771ffd33438
Johannes Gätjen <johannesgaetjen@gmail.com>
2022-03-22 21:30:24 +0100

    generate secrets with capitalization and from wiktionary list

--- generate_secrets.py
+++ generate_secrets.py
@@ -1,20 +1,26 @@
 from random import Random
+from process_vecs import only_normal_letters
 
 rnd = Random(133742069)
-n_most_frequent_words = 5000
-manual_multiples = {'jede', 'linker', 'erstes', 'letzte', 'äußerer', 'viertes', 'fünfte', 'vorderer', 'einiges',
-                    'dritte', 'zweiter', 'jegliches', 'jene', 'kein', 'sämtliche', 'irgendeiner', 'dies', 'solche',
-                    'äußerster', 'welches', 'irgendwelche', 'siebter', 'manches', 'sechste', 'neunter', 'zehntes',
-                    'derselbe', 'die', 'dasjenige', 'eine', 'ein'}
+
+early_solutions = ['eineinhalb', 'vereinbarung', 'aufeinander', 'anstieg', 'nunmehr']
 
 if __name__ == '__main__':
-    with open('data/derewo-v-ww-bll-320000g-2012-12-31-1.0.txt', 'r') as f:
+    with open('data/frequent_words.txt', 'r', encoding='UTF-8') as f:
         file_content = f.readlines()
-    words = [line.split()[0].lower() for line in file_content if line[0] != '#'][:n_most_frequent_words]
-    words = set(w for w in words if all(c in 'abcdefghijklmnopqrstuvwxyzäöüß' for c in w)).union(manual_multiples)
-    print('# words:', len(words))
+    words = set()
+    removed = set()
+    for line in file_content:
+        if only_normal_letters(line.strip(), True):
+            words.add(line.strip())
+        else:
+            removed.add(line.strip())
+    words = words.difference(early_solutions)
+    print('removed:', len(removed), removed)
     shuffle_list = list(words)
     shuffle_list.sort()
     rnd.shuffle(shuffle_list)
+    shuffle_list = early_solutions + shuffle_list
+    print('# words:', len(shuffle_list))
     with open('data/secrets.txt', 'w', encoding='utf-8') as f:
         f.write('\n'.join(shuffle_list))
